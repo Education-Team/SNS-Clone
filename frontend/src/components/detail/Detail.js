e7a3d4f167a057290cbaff02f916a6dfeca8012e
powedonk123 <powedonk123@naver.com>
2020-12-17 06:52:34 +0000

    :)

--- frontend/src/components/detail/Detail.js
+++ frontend/src/components/detail/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 import Modal from '../utils/Modal';
 import DetailHead from './DetailHead';
 import DetailImageList from './DetailImageList';
@@ -6,53 +6,52 @@ import DetailImageList from './DetailImageList';
 import DetailSide from './DetailSide';
 
 import TestImage from './TestImage.jpg';
-import './style.css'
+import './style.css';
 
 const DetailData = {
-	id : "1",
-	username : "TestName",
-	profile_img_url : TestImage,
-	text : "테스트 내용 입니다.",
-	timestamp : 1607555506, // 밀리초
-	preview_like : 3,
-	display_resources : [
+	id: '1',
+	username: 'TestName',
+	profile_img_url: TestImage,
+	text: '테스트 내용 입니다.',
+	timestamp: 1607555506, // 밀리초
+	preview_like: 3,
+	display_resources: [
 		{
-			id : 1,
-			url : TestImage
+			id: 1,
+			url: TestImage,
 		},
 		{
-			id : 2,
-			url : TestImage
+			id: 2,
+			url: TestImage,
 		},
 		{
-			id : 3,
-			url : TestImage
-		}
+			id: 3,
+			url: TestImage,
+		},
 	],
-	commnt : [
+	commnt: [
 		{
-			id : 1,
-			username : "TestName-댓글",
-			profile_img_url : TestImage,
-			liked_count : 1,
-			text : "코멘트 테스트 내용 입니다.1",
-			created_at : 1603405566		// 밀리초
+			id: 1,
+			username: 'TestName-댓글',
+			profile_img_url: TestImage,
+			liked_count: 1,
+			text: '코멘트 테스트 내용 입니다.1',
+			created_at: 1603405566, // 밀리초
 		},
 		{
-			id : 2,
-			username : "TestName-댓글",
-			profile_img_url : TestImage,
-			liked_count : 2,
-			text : "코멘트 테스트 내용 입니다.2",
-			created_at : 1603405566		// 밀리초
-		}
-	]	
-}
+			id: 2,
+			username: 'TestName-댓글',
+			profile_img_url: TestImage,
+			liked_count: 2,
+			text: '코멘트 테스트 내용 입니다.2',
+			created_at: 1603405566, // 밀리초
+		},
+	],
+};
 
 const Detail = () => {
-	
 	const [data, setData] = useState(DetailData);
-	
+
 	return (
 		<Modal>
 			<div className="dialog_container">
@@ -60,21 +59,19 @@ const Detail = () => {
 				<div className="detail_container">
 					<article className="detail_article_block">
 						<header className="detail_header_block">
-							<DetailHead data={data}/>
+							<DetailHead data={data} />
 						</header>
 						<div className="detail_image_container">
-							<DetailImageList resources={data.display_resources}/>
+							<DetailImageList resources={data.display_resources} />
 						</div>
 						<div className="detail_side_container">
-							<DetailSide data={data}/>
+							<DetailSide data={data} />
 						</div>
 					</article>
 				</div>
 			</div>
 		</Modal>
 	);
-}
+};
 
 export default Detail;
-
-
